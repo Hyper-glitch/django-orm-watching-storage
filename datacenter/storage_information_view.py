02de4beeb14f52398118ebb28240f464d3510122
Roman <graff049@gmail.com>
2022-06-19 15:48:23 +0300

    refactor for loop and filter not leaved visits

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 
-from datacenter.models import Passcard
 from datacenter.models import Visit
 
 
 def storage_information_view(request):
-    passcards = Passcard.objects.all()
-    non_closed_visits = []
+    not_leaved_visits = Visit.objects.filter(leaved_at=None)
+    who_in_storage = []
 
-    for passcard in passcards:
-        visits = Visit.objects.filter(passcard=passcard)
-        for visit in visits:
-            if not visit.leaved_at:
-                duration = visit.get_duration()
-                non_closed_visit = {
-                    'who_entered': visit.passcard.owner_name,
-                    'entered_at': visit.entered_at,
-                    'duration': visit.format_duration(duration=duration),
-                }
-                non_closed_visits.append(non_closed_visit)
+    for visit in not_leaved_visits:
+        duration = visit.get_duration()
+        non_closed_visit = {
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': visit.format_duration(duration=duration),
+        }
+        who_in_storage.append(non_closed_visit)
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': who_in_storage,
     }
     return render(request, 'storage_information.html', context)
