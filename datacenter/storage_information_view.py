73717c3b2c4008d2da38a0bdc9fe2b8a5d13d3c6
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-20 15:03:05 +0300

    refactor(rename visits)

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,20 +6,20 @@ from datacenter.models import Visit
 
 def storage_information_view(request):
     passcards = Passcard.objects.all()
-    non_closed_visits = []
+    serialized_visits = []
 
     for passcard in passcards:
         visits = Visit.objects.filter(passcard=passcard)
         for visit in visits:
             if not visit.leaved_at:
-                non_closed_visit = {
+                serialized_visit = {
                     'who_entered': visit.passcard.owner_name,
                     'entered_at': visit.entered_at,
                     'duration': visit.format_duration(),
                 }
-                non_closed_visits.append(non_closed_visit)
+                serialized_visits.append(serialized_visit)
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_visits,
     }
     return render(request, 'storage_information.html', context)
